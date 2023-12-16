d58dc79abf6549b199d211ad86b6f77941f55a68
GuillermoFuentes <guillermo.fuentes01@alumnos.ucn.cl>
2023-12-16 06:42:56 -0300

    feat: logged and register dto

---
+++ Backend/MobileHub/DTO/LoggedUserDto.cs
@@ -0,0 +1,12 @@
+namespace MobileHub.DTO
+{
+    public class LoggedUserDto
+    {
+        public string Token { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+        
+    }
+}
---
+++ Backend/MobileHub/DTO/LoginDto.cs
@@ -0,0 +1,10 @@
+namespace MobileHub.DTO
+{
+    public class LoginDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+        
+    }
+}
